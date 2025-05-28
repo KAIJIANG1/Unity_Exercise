e3e250a55545deb49053c260fddfe4be9dc81f18
在整薯条的月月鸟 <648035917@qq.com>
2025-05-29 00:25:32 +0800

    Exercise

--- Assets/Exercise.cs
+++ Assets/Exercise.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Exercise : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
